9ce8e143d08fa92f1570ec61ff150aaabfb4bc64
Astellon <22632787+astellon@users.noreply.github.com>
2018-06-18 04:00:05 +0900

    add logger class

---
+++ Source/logger.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class LogOutput {
+ public:
+  virtual LogOutput();
+  virtual log(std::string& str) = 0;
+};
+
+class ErrOut : public LogOutput {
+ public:
+  void log(std::string& str) override {
+    std::cerr << str << std::endl;
+  }
+};
+
+class Logger {
+
+ public:
+  Log() {
+    outputs_.push_back(&err);
+  }
+
+  void log(std::string& str) {
+    for (auto out : outputs_) {
+      out->log(str);
+    }
+  }
+
+ private:
+  ErrOut err;
+  std::vector<LogOutput*> outputs_;
+};
