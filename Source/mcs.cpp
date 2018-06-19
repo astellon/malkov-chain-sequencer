0f2d5ccb8ccef267c8b53bfbcef50cb9948067d4
Astellon <22632787+astellon@users.noreply.github.com>
2018-06-19 16:21:08 +0900

    finish read MIDI

--- Source/mcs.cpp
+++ Source/mcs.cpp
@@ -45,16 +45,30 @@ void MalkovChainSequencer::analyse() {
   for (int i = 0; i != num_tracks; i++) {
     log("Read Track: " + std::to_string(i));
     auto mseq = midi_.getTrack(i);
-    auto num_events = mseq->getNumEvents();
-    for (int j = 0; j != num_events; j++) {
-      auto msg = mseq->getEventPointer(j)->message;
-      if (msg.isNoteOn()) {
-        log("NOTEON: NoteNumber:" + std::to_string(msg.getNoteNumber()) + " at: " + std::to_string(msg.getTimeStamp()));
-      } else if (msg.isNoteOff()) {
-        log("NOTEOFF: NoteNumber:" + std::to_string(msg.getNoteNumber()) + " at: " + std::to_string(msg.getTimeStamp()));
-      } else if (msg.isTempoMetaEvent()) {
-        seconds_per_quarter_note = msg.getTempoSecondsPerQuarterNote();
-        log("TEMPO: Sec/QuarterNote: " + std::to_string(seconds_per_quarter_note));
+    MidiMessageSequence achan;
+    for (int j = 1; j != 17; j++) {
+      log("Read channel: " + std::to_string(j));
+      achan.clear();
+      mseq->extractMidiChannelMessages(j, achan, true);
+      auto num_events = achan.getNumEvents();
+      for (int k = 0; k != num_events; k++) {
+        auto msg = achan.getEventPointer(k)->message;
+        if (msg.isNoteOn()) {
+          log("  NOTEON: NoteNumber: "
+            + msg.getMidiNoteName(msg.getNoteNumber(), true, true, 4).toStdString()
+            + " at: " + std::to_string(msg.getTimeStamp())
+          );
+        } else if (msg.isNoteOff()) {
+          log(" NOTEOFF: NoteNumber: "
+            + msg.getMidiNoteName(msg.getNoteNumber(), true, true, 4).toStdString()
+            + " at: " + std::to_string(msg.getTimeStamp())
+          );
+        } else if (msg.isTempoMetaEvent()) {
+          seconds_per_quarter_note = msg.getTempoSecondsPerQuarterNote();
+          log("   TEMPO: Sec/QuarterNote: "
+            + std::to_string(seconds_per_quarter_note)
+          );
+        }
       }
     }
   }
